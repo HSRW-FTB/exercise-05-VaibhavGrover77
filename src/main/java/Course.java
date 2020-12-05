eb5a3ea41d348873ed69f8898123ac09dd57065a
VaibhavGrover77 <73877358+VaibhavGrover77@users.noreply.github.com>
2020-12-05 23:06:18 +0100

    Update Course.java

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,1 +1,39 @@
-enum Course { ME, MSE, EL, IE, BMS, SCB, MME, MBB }
+//enum Course { ME, MSE, EL, IE, BMS, SCB, MME, MBB }
+public class Course extends User{
+    private String id;
+    private String name;
+    private Professor lecturer;
+    private int credit;
+
+    private Course(){
+    }
+
+    public Course(String id, String name, Professor lecturer, int credit){
+        this.id = id;
+        this.name = name;
+        this.lecturer = lecturer;
+        this.credit = credit;
+    }
+
+    public String getID(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setLecturer(Professor lecturer){
+        this.lecturer = lecturer;
+    }
+    public Professor getLecturer(){
+        return lecturer;
+    }
+    public int getCredit(){
+        return credit;
+    }
+    public void setCredit(int credit){
+        this.credit = credit;
+    }
+}

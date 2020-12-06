fdf5e6a365d1a40e04bb852f936037302c64cffd
VaibhavGrover77 <73877358+VaibhavGrover77@users.noreply.github.com>
2020-12-06 23:42:30 +0100

    Update Classroom.java

--- src/main/java/Classroom.java
+++ src/main/java/Classroom.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.Calender;
+impport java.util.GregorianCalender;
 import java.util.Date;
+
 public class Classroom {
     private String id;
     private Course course;
-    private Student[] participants;
+    private ArrayList<Student> participants = new ArrayList<Student>();
     private String room;
     private String term;
     //private Date date;
@@ -12,12 +16,30 @@ public class Classroom {
     public Classroom(Course course, String room){
         this.course = course;
         this.room = room;
+        Date date = new Date();
+        Calender calender = new GregorianCalender();
+        int year = Calender.get(Calender.YEAR);
+        int month = calender.get(Calender.MONTH) + 1;
+        if (month<10 && month>2){
+            term = "SS" + year + "/" + ((year +1 ) %100);}
+        else{ term = "WS" + year + "/" + ((year +1 ) %100);}
+        this.id = course.getID()+ "-" + term;
     }
 
     public Classroom(Course course, String room, Date date){
         this.course = course;
         this.room = room;
-        //this.date = date ; 
+        Calender calender = new GregorianCalender();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        if (month<10 & month >2){
+            term = "SS"+year+"/"+((year+1)%100);}
+        else{
+            term = "WS"+year+"/"+((year+1)%100);}
+        
+        this.id=course.getID()+"-"+ term;
+        
     }
     public Course getCourse(){
         return course ;
@@ -36,8 +58,17 @@ public class Classroom {
     }
     
     // add student 
+    public void addStudent(Student student){
+        participant.add(student);}
 
     public String getParticipantsEmail(){
-        return ;
+        String emails = "";
+         for (int i=0; i < participants.size();i++){
+            emails = emails + participants.get(i).getFQUN() + ", ";
+        }
+        return emails.substring(0, emails.length() - 2);
+    }
+    public int getTotalParticipants(){
+        return participants.size();
     }
 }
